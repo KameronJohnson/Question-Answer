03db2b887293978b3446c69bc36794d867d8859b
kameron johnson <kameronjohnson@gmail.com>
2018-01-29 18:58:56 -0800

    add body-parser & use  es6 syntax

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 
 const express = require('express');
 const app = express();
+const jsonParser = require('body-parser');
+
 const port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    console.log("Hello Middleware");
+    next();
+});
 
 //start server
 app.listen(port, function() {
