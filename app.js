c2150de3b9bbd3dd3c18d444ae95dce6c86b9d83
kameron johnson <kameronjohnson@gmail.com>
2018-01-30 16:56:34 -0800

    add answer, vote up and vote down routes with error handling

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var express = require('express');
-var jsonParser = require('body-parser').json;
 var app = express();
 var routes = require('./routes');
+
+var jsonParser = require('body-parser').json;
+var logger = require('morgan');
+
+app.use(logger("dev"));
 
 var port = process.env.PORT || 3000;
 
@@ -13,6 +17,23 @@ var port = process.env.PORT || 3000;
 app.use(jsonParser());
 
 app.use("/questions", routes);
+
+//catch 404
+app.use(function(req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// Error Handler
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    })
+});
 
 //start server
 app.listen(port, function() {
--- routes.js
+++ routes.js
@@ -21,13 +21,63 @@ router.post("/", function(req, res) {
     });
 });
 
-//GET /questions/id
+//GET /questions/qID
 //Route for specific questions
-router.get("/:id", function(req, res) {
+router.get("/:qID", function(req, res) {
     // return all the questions
     res.json({
-        response: "You sent me a GET request for ID " + req.params.id
+        response: "You sent me a GET request for qID " + req.params.id
     });
 });
 
+//POST /questions/:qID/answers
+//Route for creating an answer
+router.post("/:qID/answers", function(req, res) {
+    res.json({
+        response: "You sent me a POST request to /answers",
+        questionId: req.params.qID,
+        body: req.body
+    });
+});
+
+//PUT /questions/:qID/answers/:aID
+//Edit a specific answer
+router.put("/:qID/answers/:aID", function(req, res) {
+    res.json({
+        response: "You sent me a PUT request to update aID " + req.params.aID,
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        body: req.body
+    });
+});
+
+//DELETE /questions/:id/answers/:id
+//Delete a specific answer
+router.delete("/:qID/answers/:aID", function(req, res) {
+    res.json({
+        response: "You sent me a DELETE request for aID " + req.params.aID,
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+    });
+});
+
+//POST /questions/:id/answers/:id/vote-up
+//POST /questions/:id/answers/:id/vote-down
+//Upvote an answer
+router.post("/:qID/answers/:aID/vote-:dir", function(req, res, next) {
+        if(req.params.dir.search(/^(up|down)$/) === -1) {
+            var err = new Error("Not Found");
+            err.status = 404;
+            next(err);
+        } else {
+            next();
+        }
+    },function(req, res) {
+    res.json({
+        response: "You sent me a POST request to /vote " + req.params.dir,
+        questionId: req.params.qID,
+        answerId: req.params.aID,
+        vote: req.params.dir
+    });
+});
 module.exports = router;
